c478532c7f1f48d010845b0d1c988ea9cb199d1d
Timo Anttila <info@tuspe.com>
2020-03-26 19:58:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,18 @@
 <div id="prices">
   <label for='sizes'>Valitse koko</label>
   <select id='sizes' name='extra_sizes' class='custom' onchange='changePrice(this)'>
-  <?php foreach($page->shop_sizes_price as $item) echo "<option value='". base64_encode(json_encode(array("title" => $item->title, "price" => $item->shop_price))) ."'>". $item->shop_sizes->first->title .", $item->shop_price €</option>"; ?>
+  <?php
+    foreach($page->shop_sizes_price as $item){
+      echo "<option value='". base64_encode(json_encode(array(
+        "title" => $item->title,
+        "price" => $item->shop_price
+      ))) ."'>". $item->shop_sizes->first->title .", $item->shop_price €</option>"; ?>
   </select>
 </div>
 
-<script>function changePrice(el) { var item = JSON.parse(atob(el.value)); document.getElementById("main_price").innerHTML = item["price"].toFixed(2); }</script>
+<script>
+  function changePrice(el) {
+    var item = JSON.parse(atob(el.value));
+    document.getElementById("main_price").innerHTML = item["price"].toFixed(2);
+  }
+</script>
